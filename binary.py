b0d52c62fce87e1d8206f7421a165b8212437642
Agnes-Kamondi <kamondiagnesauma@gmail.com>
2024-04-01 13:04:57 +0300

    Example 2

--- binary.py
+++ binary.py
@@ -1,3 +1,4 @@
+# Example 1
 class TreeNode:
     def __init__(self, data):
         self.data = data
@@ -33,3 +34,48 @@ node6.left = node7
 
 preOrderTraversal(root)
 
+# Example 2
+class Node:
+   def __init__(self, data):
+      self.left = None
+      self.right = None
+      self.data = data
+
+   def insert(self, data):
+      if self.data:
+         if data < self.data:
+            if self.left is None:
+               self.left = Node(data)
+            else:
+               self.left.insert(data)
+         elif data > self.data:
+            if self.right is None:
+               self.right = Node(data)
+            else:
+               self.right.insert(data)
+         else:
+            self.data = data
+
+   def PrintTree(self):
+      if self.left:
+         self.left.PrintTree()
+      print( self.data),
+      if self.right:
+         self.right.PrintTree()
+
+
+   def PreorderTraversal(self, root):
+      res = []
+      if root:
+         res.append(root.data)
+         res = res + self.PreorderTraversal(root.left)
+         res = res + self.PreorderTraversal(root.right)
+      return res
+root = Node(27)
+root.insert(14)
+root.insert(35)
+root.insert(10)
+root.insert(19)
+root.insert(31)
+root.insert(42)
+print(root.PreorderTraversal(root))
